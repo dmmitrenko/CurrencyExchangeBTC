9e92499dab941f56bcf8a2d9a26d51e41b38c453
Serhii Dmytrenko <dmmytrenko@gmail.com>
2023-05-25 23:46:11 +0300

    email validation added

--- GSES2.Api/Controllers/SubscriptionController.cs
+++ GSES2.Api/Controllers/SubscriptionController.cs
@@ -1,6 +1,9 @@
-﻿using GSES2.Domain.Transport.Requests;
+﻿using GSES2.Domain.Exceptions;
+using GSES2.Domain.Transport.Requests;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
 
 namespace GSES2.Api.Controllers;
 public class SubscriptionController : Controller
@@ -10,8 +13,13 @@ public class SubscriptionController : Controller
     public SubscriptionController(IMediator mediator) => _mediator = mediator;
     
     [HttpPost("subscribe")]
-    public async Task<IActionResult> SubscribeEmail([FromQuery] string email)
+    public async Task<IActionResult> SubscribeEmail([FromQuery] [EmailAddress] string email)
     {
+        if (!ModelState.IsValid)
+        {
+            throw new DomainException("Email incorrect!", (int)HttpStatusCode.BadRequest);
+        }
+
         await _mediator.Send(new SubscribeEmailRequest { Email = email });
         return CreatedAtAction(nameof(SubscribeEmail), email, null);
     }
